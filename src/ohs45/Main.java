7b00fe302afcc3ecb7a720fbb233cccfc70ff610
rmcmonigle <rmcmonigle@OHSCAD-TEACHER.orange.local>
2015-05-22 08:23:02 -0400

    I added my name!

--- src/ohs45/Main.java
+++ src/ohs45/Main.java
@@ -10,6 +10,7 @@ package ohs45;
  * @author Ryan Rule-Hoffman <ryan.rulehoffman@icloud.com>
  * @author Simon Wieder <16rwieder@orangecsd.org>
  * @author Michael Litt <17mlitt@orangecsd.org>
+ * @author Mr. McMonigle <rmcmonigle@orangecsd.org>
  */
 public class Main
 {
