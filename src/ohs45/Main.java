43699d63992e1d5cf712bf34f9819dadb8d111a7
Simon <Simon@10.93.228.88>
2015-05-22 11:37:45 -0400

    -Changed the width back to 350 after investigating some merge conflicts.

--- src/ohs45/Main.java
+++ src/ohs45/Main.java
@@ -21,7 +21,7 @@ public class Main
     /**
      * The width of the game.
      */
-    private static final int WIDTH = 300;
+    private static final int WIDTH = 350;
     
     /**
      * The height of the game.
